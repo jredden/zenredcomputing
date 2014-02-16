5c8eec2cdee94b0eb69eb77d0cdafd2df7e8cec2
jredden <jredden@jredden-laptop>
2014-02-15 22:52:11 -0800

    in progress

--- src/main/sql/zenredcomputing.dfn.sql
+++ src/main/sql/zenredcomputing.dfn.sql
@@ -17,10 +17,58 @@ DROP TABLE IF EXISTS `Subjects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Subjects` (
-`Subject_id` int unsigned NOT NULL auto_increment
-,`Subject_code` int unsigned NOT NULL
-,`Subject_name` varchar(255) NOT NULL default ''
+`Subjects_id` int unsigned NOT NULL auto_increment
+,`Subjects_code` int unsigned NOT NULL
+,`Subjects_name` varchar(255) NOT NULL default ''
 ,`Datestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-,KEY `Subjects_idKey` (`Subject_id`)
+,KEY `Subjects_idKey` (`Subjects_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `Posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Posts` (
+`Posts_id` int unsigned NOT NULL auto_increment
+,`Content` text
+,`Datestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+,KEY `Posts_idKey` (`Posts_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `UserToSubjects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UserToSubjects` (
+`UserToSubjects_id` int unsigned NOT NULL auto_increment
+,`Subjects_id` int unsigned NOT NULL
+,`User_id` int unsigned NOT NULL
+,`Datestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+,KEY `UsersToSubjects_idKey` (`UserToSubjects_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `UserToPosts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UserToPosts` (
+`UserToPosts_id` int unsigned NOT NULL auto_increment
+,`Subjects_id` int unsigned NOT NULL
+,`Posts_id` int unsigned NOT NULL
+,`Datestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+,KEY `UsersToPosts_idKey` (`UserToPosts_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `SubjectsToPosts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SubjectsToPosts` (
+`SubjectsToPosts_id` int unsigned NOT NULL auto_increment
+,`Subjects_id` int unsigned NOT NULL
+,`Posts_id` int unsigned NOT NULL
+,`Datestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+,KEY `SubjectsToPosts_idKey` (`SubjectsToPosts_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
