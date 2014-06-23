929b732114544b07c837940e7e03f3c4ecec8f17
jredden <jredden@jredden-laptop>
2014-06-23 13:25:38 -0700

    blog construction

--- src/main/java/com/zenred/zenredcomputing/controller/Category.java
+++ src/main/java/com/zenred/zenredcomputing/controller/Category.java
@@ -6,12 +6,24 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.zenred.zenredcomputing.controller.json.SubjectsView;
+import com.zenred.zenredcomputing.domain.SubjectsDAO;
+import com.zenred.zenredcomputing.vizualization.SubjectsResponse;
+
 public class Category implements Controller {
+	
+	private SubjectsDAO subjectsDAO;
 
 	public ModelAndView handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		subjectsDAO = new SubjectsDAO();
+		String [] subjects = subjectsDAO.readSubjects().toArray(new String []{});
+		SubjectsResponse subjectsResponse = new SubjectsResponse();
+		subjectsResponse.setSubjects(subjects);
+		ModelAndView modelAndView = new ModelAndView(new SubjectsView());
+		modelAndView.addObject(SubjectsView.JSON_ROOT, subjectsResponse);
+		return modelAndView;
 	}
 
 }
