57a5ec75748ddb3177e93df9be0234179e053c25
jredden <johnredden@aol.com>
2017-03-10 20:13:18 -1000

    zenredcomputing to ubuntu 16.04 4

--- src/main/sql/Subjects.sql
+++ src/main/sql/Subjects.sql
@@ -1,5 +1,5 @@
 INSERT INTO `Subjects` (`Subjects_code`, `Subjects_name`) VALUES (10, 'Computer History');
 INSERT INTO `Subjects` (`Subjects_code`, `Subjects_name`) VALUES (100, 'Computer Languages');
-INSERT INTO `Subjects` (`Subjects_code`, `Subjects_name`) VALUES (100, 'Inclusive Software Engineering');
-INSERT INTO `Subjects` (`Subjects_code`, `Subjects_name`) VALUES (110, 'Business Stories');
+INSERT INTO `Subjects` (`Subjects_code`, `Subjects_name`) VALUES (1000, 'Inclusive Software Engineering');
+INSERT INTO `Subjects` (`Subjects_code`, `Subjects_name`) VALUES (1100, 'Business Stories');
 
